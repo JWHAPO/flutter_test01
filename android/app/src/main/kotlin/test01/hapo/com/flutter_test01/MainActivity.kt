eeca0b88313417cb71fcd74dbe2407c9167672c7
JWHAPO <kim2101714@gmail.com>
2019-05-14 17:47:53 +0900

    init test flutter app

---
+++ android/app/src/main/kotlin/test01/hapo/com/flutter_test01/MainActivity.kt
@@ -0,0 +1,13 @@
+package test01.hapo.com.flutter_test01
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
