aaac942cf3e2559eaf6e42e826d2bf045617c5d5
김정우 <jungwoo@AkasimaMac-1192.local>
2019-05-26 19:42:40 +0900

    add onBackPressed method

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -21,6 +20,7 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatefulWidget {
   final String title;
+
   HomePage(this.title);
 
   @override
@@ -40,23 +40,21 @@ class _HomePageState extends State<HomePage> {
 
     //권한을 설정
     _messaging.requestNotificationPermissions(
-      const IosNotificationSettings(sound: true, badge: true, alert: true)
-    );
-    _messaging.onIosSettingsRegistered.listen((IosNotificationSettings settings){
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _messaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
       print('Setting s registered: $settings');
     });
 
     //PlatformException 발생 시 flutter clean 하고 flutter packages get 한번 돌려봐라
-    _messaging.getToken().then((token){
-      print('firebase token:'+token);
+    _messaging.getToken().then((token) {
+      print('firebase token:' + token);
     });
 
     _messaging.configure(
       onMessage: (Map<String, dynamic> message) async {
         print('onResume $message');
-        setState(() {
-
-        });
+        setState(() {});
       },
       onResume: (Map<String, dynamic> message) async {
         final notification = message['data'];
@@ -73,79 +71,90 @@ class _HomePageState extends State<HomePage> {
 
   final List<String> _urls = [
     'https://www.lavalsehotel.com',
-    'http://www.president.go.kr/',
+    'https://www.apple.com/kr/',
     'https://www.google.com',
-    'https://www.naver.com',
+    'https://m.naver.com',
     'https://en.m.wikipedia.org/'
   ];
 
-  launchUrl(){
-      this.webViewController.loadUrl(this._urls[_currentIndex]);
+  launchUrl() {
+    this.webViewController.loadUrl(this._urls[_currentIndex]);
+  }
+
+  Future<bool> _onBackPressed() {
+    return showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text("Do you want to exit?"),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('No'),
+                  onPressed: () => Navigator.pop(context, false),
+                ),
+                FlatButton(
+                  child: Text('Yes'),
+                  onPressed: () => Navigator.pop(context, true),
+                )
+              ],
+            ));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          NavigationControls(_controller.future),
-          PopupMenuButton<String>(
-           onSelected: choiceAction,
-           itemBuilder: (BuildContext context){
-             return Constants.choices.map((String choice){
-              return PopupMenuItem<String>(child: Text(choice),value: choice,);
-             }).toList();
-           },
-          )
-        ],
-      ),
-      body: WebView(
-        initialUrl: _urls[_currentIndex],
-        onWebViewCreated: (WebViewController webViewController) {
-          this.webViewController = webViewController;
-          _controller.complete(webViewController);
-        },
-        javascriptMode: JavascriptMode.unrestricted,
-        navigationDelegate: (NavigationRequest request){
-          if(request.url.startsWith('https://www.naver.')){
-            print('LaValse');
-            return NavigationDecision.prevent;
-          }
-
-          print('gogo');
-          return NavigationDecision.navigate;
-
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-          onTap: onTabTapped,
-          currentIndex: _currentIndex,
-          type: BottomNavigationBarType.fixed,
-          items:[
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text('Main')
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.assignment_late),
-                title: Text('Notice')
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.calendar_today),
-                title: Text('Book')
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.developer_board),
-                title: Text('Board')
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.help_outline),
-                title: Text('Inquery'),
-            ),
-          ]
-      ),
-    );
+    return WillPopScope(
+        onWillPop: _onBackPressed,
+        child: new Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            actions: <Widget>[
+              NavigationControls(_controller.future),
+              PopupMenuButton<String>(
+                onSelected: choiceAction,
+                itemBuilder: (BuildContext context) {
+                  return Constants.choices.map((String choice) {
+                    return PopupMenuItem<String>(
+                      child: Text(choice),
+                      value: choice,
+                    );
+                  }).toList();
+                },
+              )
+            ],
+          ),
+          body: WebView(
+            initialUrl: _urls[_currentIndex],
+            onWebViewCreated: (WebViewController webViewController) {
+              this.webViewController = webViewController;
+              _controller.complete(webViewController);
+            },
+            javascriptMode: JavascriptMode.unrestricted,
+            navigationDelegate: (NavigationRequest request) {
+              if (request.url.startsWith('https://www.naver.')) {
+                // print('LaValse');
+                // return NavigationDecision.prevent;
+              }
+              return NavigationDecision.navigate;
+            },
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+              onTap: onTabTapped,
+              currentIndex: _currentIndex,
+              type: BottomNavigationBarType.fixed,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home), title: Text('Main')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.assignment_late), title: Text('Notice')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.calendar_today), title: Text('Book')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.developer_board), title: Text('Board')),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.help_outline),
+                  title: Text('Inquery'),
+                ),
+              ]),
+        ));
   }
 
   void onTabTapped(int index) {
@@ -155,36 +164,43 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void choiceAction(String choice){
-    if(choice == Constants.Settings){
+  void choiceAction(String choice) {
+    if (choice == Constants.Settings) {
       this.webViewController.loadUrl('https://www.daum.net/');
-    }else if(choice == Constants.Subscribe){
-      print('Subscribe'); 
+    } else if (choice == Constants.Subscribe) {
+      print('Subscribe');
     }
-    
   }
 }
 
 class NavigationControls extends StatelessWidget {
   final Future<WebViewController> _webViewcontrollerFuture;
 
-  const NavigationControls(this._webViewcontrollerFuture) : assert(_webViewcontrollerFuture != null);
+  const NavigationControls(this._webViewcontrollerFuture)
+      : assert(_webViewcontrollerFuture != null);
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<WebViewController>(
       future: _webViewcontrollerFuture,
-      builder: (BuildContext context, AsyncSnapshot<WebViewController> snapshot){
-        final  bool webViewReady = snapshot.connectionState ==ConnectionState.done;
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
+        final bool webViewReady =
+            snapshot.connectionState == ConnectionState.done;
         final WebViewController controller = snapshot.data;
         return Row(
           children: <Widget>[
             IconButton(
               icon: const Icon(Icons.arrow_back_ios),
-              onPressed: !webViewReady?null:()=>navigate(context, controller, goBack: true),
+              onPressed: !webViewReady
+                  ? null
+                  : () => navigate(context, controller, goBack: true),
             ),
             IconButton(
               icon: const Icon(Icons.arrow_forward_ios),
-              onPressed: !webViewReady?null:()=>navigate(context, controller, goBack: false),
+              onPressed: !webViewReady
+                  ? null
+                  : () => navigate(context, controller, goBack: false),
             )
           ],
         );
@@ -192,16 +208,17 @@ class NavigationControls extends StatelessWidget {
     );
   }
 
-  navigate(BuildContext context, WebViewController controller, {bool goBack: false}) async{
-    bool canNavigate = goBack ? await controller.canGoBack() : await controller.canGoForward();
-    if(canNavigate) {
+  navigate(BuildContext context, WebViewController controller,
+      {bool goBack: false}) async {
+    bool canNavigate =
+        goBack ? await controller.canGoBack() : await controller.canGoForward();
+    if (canNavigate) {
       goBack ? controller.goBack() : controller.goForward();
-    }else{
+    } else {
       Scaffold.of(context).showSnackBar(
-        SnackBar(content: Text("No ${goBack ? 'back' : 'foward'} history item")),
+        SnackBar(
+            content: Text("No ${goBack ? 'back' : 'foward'} history item")),
       );
     }
   }
-
 }
-
