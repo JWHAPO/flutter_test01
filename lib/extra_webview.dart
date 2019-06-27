24d7de8cc1d518d8f166e9ced85284f3502fc3bb
JWHAPO <kim2101714@gmail.com>
2019-06-27 13:30:04 +0900

    add navigation pop :

--- lib/extra_webview.dart
+++ lib/extra_webview.dart
@@ -1,33 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class ExtraWebView extends StatelessWidget {
-
+class ExtraWebView extends StatefulWidget {
   final String url;
+
   ExtraWebView({Key key, @required this.url}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  _ExtraWebViewState createState() => _ExtraWebViewState();
+}
 
+class _ExtraWebViewState extends State<ExtraWebView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: WebView(
-            initialUrl: url,
-            onWebViewCreated: (WebViewController webViewController) {
-            },
-            javascriptMode: JavascriptMode.unrestricted,
-            onPageFinished: (String url) async {
-
-            },
-            gestureRecognizers: Set()
-                ..add(
-                Factory<VerticalDragGestureRecognizer>(
-                () => VerticalDragGestureRecognizer(),
+        body: SafeArea(
+          bottom: false,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Flexible(
+                flex: 10,
+                child: WebView(
+                  initialUrl: widget.url,
+                  onWebViewCreated: (WebViewController webViewController) {},
+                  javascriptMode: JavascriptMode.unrestricted,
+                  navigationDelegate: (NavigationRequest request) {
+                    if (request.url.startsWith('###')) {
+                      return NavigationDecision.prevent;
+                    }
+                    return NavigationDecision.navigate;
+                  },
+                  onPageFinished: (String url) {},
+                  gestureRecognizers: Set()
+                    ..add(
+                      Factory<VerticalDragGestureRecognizer>(
+                        () => VerticalDragGestureRecognizer(),
+                      ),
+                    ),
                 ),
-            ),
+              ),
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                      color: Colors.white30,
+                      padding: EdgeInsets.all(10.0),
+                      alignment: Alignment.centerRight,
+                      child: IconButton(
+                        icon: Icon(Icons.close),
+                        onPressed: () {
+                          Navigator.pop(context,"1");
+                        },
+                      )))
+            ],
           ),
+        ),
       ),
     );
   }
