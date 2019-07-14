c529f713bd3a28745e223447bf740076d3692cda
JWHAPO <kim2101714@gmail.com>
2019-07-14 23:09:47 +0900

    add bouncing button animation

--- lib/login.dart
+++ lib/login.dart
@@ -5,20 +5,50 @@ class Login extends StatefulWidget {
   _LoginState createState() => _LoginState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginState extends State<Login> with SingleTickerProviderStateMixin{
 
   var _opacity = 0.0;
   var _id = '';
   var _pw = '';
+  double _scale;
+  AnimationController _controller;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200),
+      lowerBound: 0.0,
+      upperBound: 0.1
+    )..addListener((){
+      setState(() {
+      });
+    });
+
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onTabDown(TapDownDetails details){
+    _controller.forward();
+  }
+
+  void _onTabUp(TapUpDetails details){
+    _controller.reverse();
   }
 
   @override
   Widget build(BuildContext context) {
+
+    _scale = 1- _controller.value;
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
@@ -64,7 +94,13 @@ class _LoginState extends State<Login> {
                  _opacity = _opacity==0.0?1.0:0.0; 
                 });
               },
-            ),Wrap(
+            ),
+            GestureDetector(
+              onTapDown: _onTabDown,
+              onTapUp: _onTabUp,
+              child: Transform.scale(scale: _scale,child: _animatedButtonUI,),
+            ),
+            Wrap(
               spacing: 8.0, // gap between adjacent chips
               runSpacing: 4.0, // gap between lines
               children: <Widget>[
@@ -111,4 +147,28 @@ class _LoginState extends State<Login> {
       ),
     );
   }
+
+  Widget get _animatedButtonUI => Container(
+    height: 100,
+    width: 250,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(100),
+      boxShadow: [ BoxShadow(
+        color: Colors.grey,
+        blurRadius: 20.0
+      ) ],
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Color(0xFFA7BFE8),
+          Color(0xFF6190E8),
+        ],
+      ),
+    ),
+    child: Center(
+      child: Text('Tab', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,color: Colors.white),),
+    ),
+  );
+
 }
