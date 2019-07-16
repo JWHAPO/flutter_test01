2d1fb29d744baac4f332af3357e8142ce6b15a77
JWHAPO <kim2101714@gmail.com>
2019-07-16 21:16:26 +0900

    add singlechildscrollview

--- lib/login.dart
+++ lib/login.dart
@@ -58,97 +58,99 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin{
             elevation: 2.0,
           ),
         body: SafeArea(
-          child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Draggable(
-              child: Text('Dragable',style: TextStyle(color: Colors.blue),),
-              feedback: Text('Dragable',style: TextStyle(color: Colors.red),),
-              childWhenDragging: Text('Dragable',style: TextStyle(color: Colors.green),),
-            ),
-            RichText(text: TextSpan(
-             style: DefaultTextStyle.of(context).style,
-             children: <TextSpan>[
-               TextSpan(text: 'It''s ', style: TextStyle(color: Colors.blue, fontSize: 6.0)),
-               TextSpan(text: 'Login', style: TextStyle(color: Colors.blueAccent, fontSize: 10.0)),
-               TextSpan(text: 'Page.', style: TextStyle(color: Colors.blue, fontSize: 6.0)),
-             ]
-          )),
-            TextField(
-              enabled: true,
-              maxLength: 20,
-              style: TextStyle(color: Colors.black, fontFamily: 'Poppins'),
-              onChanged: (_id) {
-                this._id = _id;
-              },
-            ),
-            TextField(
-              enabled: true,
-              maxLength: 20,
-              obscureText: true,
-              style: TextStyle(color: Colors.black, fontFamily: 'Poppins'),
-              onChanged: (_pw){
-                this._pw = _pw;
-              },
-            ),
-            RaisedButton(
-              child: Text('Login', style: TextStyle(color: Colors.black, fontFamily: 'Poppins'),),
-              onPressed: (){
-
-                print('id:$_id , pw:$_pw');
-                setState(() {
-                 _opacity = _opacity==0.0?1.0:0.0; 
-                });
-              },
-            ),
-            GestureDetector(
-              onTapDown: _onTabDown,
-              onTapUp: _onTabUp,
-              child: Transform.scale(scale: _scale,child: _animatedButtonUI,),
-            ),
-            Wrap(
-              spacing: 8.0, // gap between adjacent chips
-              runSpacing: 4.0, // gap between lines
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Container(
-                  child: Chip(
-                    label: Text('Hamilton',style: TextStyle(color: Colors.white),),
-                    backgroundColor: Colors.deepPurpleAccent,
-                    elevation: 4,
-                    shadowColor: Colors.grey[50],
-                    padding: EdgeInsets.all(4),
+                Draggable(
+                  child: Text('Dragable',style: TextStyle(color: Colors.blue),),
+                  feedback: Text('Dragable',style: TextStyle(color: Colors.red),),
+                  childWhenDragging: Text('Dragable',style: TextStyle(color: Colors.green),),
+                ),
+                RichText(text: TextSpan(
+                    style: DefaultTextStyle.of(context).style,
+                    children: <TextSpan>[
+                      TextSpan(text: 'It''s ', style: TextStyle(color: Colors.blue, fontSize: 6.0)),
+                      TextSpan(text: 'Login', style: TextStyle(color: Colors.blueAccent, fontSize: 10.0)),
+                      TextSpan(text: 'Page.', style: TextStyle(color: Colors.blue, fontSize: 6.0)),
+                    ]
+                )),
+                TextField(
+                  enabled: true,
+                  maxLength: 20,
+                  style: TextStyle(color: Colors.black, fontFamily: 'Poppins'),
+                  onChanged: (_id) {
+                    this._id = _id;
+                  },
+                ),
+                TextField(
+                  enabled: true,
+                  maxLength: 20,
+                  obscureText: true,
+                  style: TextStyle(color: Colors.black, fontFamily: 'Poppins'),
+                  onChanged: (_pw){
+                    this._pw = _pw;
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Login', style: TextStyle(color: Colors.black, fontFamily: 'Poppins'),),
+                  onPressed: (){
+
+                    print('id:$_id , pw:$_pw');
+                    setState(() {
+                      _opacity = _opacity==0.0?1.0:0.0;
+                    });
+                  },
+                ),
+                GestureDetector(
+                  onTapDown: _onTabDown,
+                  onTapUp: _onTabUp,
+                  child: Transform.scale(scale: _scale,child: _animatedButtonUI,),
+                ),
+                Wrap(
+                  spacing: 8.0, // gap between adjacent chips
+                  runSpacing: 4.0, // gap between lines
+                  children: <Widget>[
+                    Container(
+                      child: Chip(
+                        label: Text('Hamilton',style: TextStyle(color: Colors.white),),
+                        backgroundColor: Colors.deepPurpleAccent,
+                        elevation: 4,
+                        shadowColor: Colors.grey[50],
+                        padding: EdgeInsets.all(4),
+                      ),
+                      margin: EdgeInsets.only(left: 12, right: 12,top: 2,bottom: 2),
+                    ),
+                    Chip(
+                      avatar: CircleAvatar(backgroundColor: Colors.blue.shade900, child: Text('ML')),
+                      label: Text('Lafayette'),
+                    ),
+                    Chip(
+                      avatar: CircleAvatar(backgroundColor: Colors.blue.shade900, child: Text('HM')),
+                      label: Text('Mulligan'),
+                    ),
+                    Chip(
+                      avatar: CircleAvatar(backgroundColor: Colors.blue.shade900, child: Text('JL')),
+                      label: Text('Laurens'),
+                    ),
+                  ],
+                ),AnimatedOpacity(
+                  duration: Duration(seconds: 1),
+                  opacity: _opacity,
+
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Align(
+                      alignment: Alignment.center,
+                      heightFactor: 0.5,
+                      widthFactor: 0.5,
+                      child: Image.asset('assets/image/potato.jpg',alignment: Alignment.center,fit: BoxFit.contain,),
+                    ),
                   ),
-                  margin: EdgeInsets.only(left: 12, right: 12,top: 2,bottom: 2),
-                ),
-                Chip(
-                  avatar: CircleAvatar(backgroundColor: Colors.blue.shade900, child: Text('ML')),
-                  label: Text('Lafayette'),
-                ),
-                Chip(
-                  avatar: CircleAvatar(backgroundColor: Colors.blue.shade900, child: Text('HM')),
-                  label: Text('Mulligan'),
-                ),
-                Chip(
-                  avatar: CircleAvatar(backgroundColor: Colors.blue.shade900, child: Text('JL')),
-                  label: Text('Laurens'),
-                ),
+                )
               ],
-            ),AnimatedOpacity(
-              duration: Duration(seconds: 1),
-              opacity: _opacity,
-              
-              child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Align(
-                alignment: Alignment.center,
-                heightFactor: 0.5,
-                widthFactor: 0.5,
-                child: Image.asset('assets/image/potato.jpg',alignment: Alignment.center,fit: BoxFit.contain,),
-              ),
             ),
-            )
-          ],
-        ),
+          ),
         )
       ),
     );
