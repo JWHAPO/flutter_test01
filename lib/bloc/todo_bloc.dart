a7e748ccbbd16f7ec7547e7c9a0e9b08c6e1beef
JWHAPO <kim2101714@gmail.com>
2019-07-19 23:00:19 +0900

    add bloc pattern

---
+++ lib/bloc/todo_bloc.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import '../model/todo.dart';
+import '../repository/api.dart';
+
+class TodoBloc{
+
+  final API api;
+
+  StreamController<List<Todo>> ctrl = StreamController();
+
+  Stream<List<Todo>> get results => ctrl.stream;
+
+  TodoBloc(this.api);
+
+  void dispose(){
+    ctrl.close();
+  }
+
+  void getTodo(){
+    api.getTodo().then((todos){
+      ctrl.add(todos);
+    });
+  }
+
+
+}
